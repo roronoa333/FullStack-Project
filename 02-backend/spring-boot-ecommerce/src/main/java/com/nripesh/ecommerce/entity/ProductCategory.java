af719607f53b7d33bba7803a332ee401bafdb9c1
Nripesh Pokala <nripeshd.ace@gmail.com>
2023-10-19 23:58:10 -0600

    fixes

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/nripesh/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,23 @@
+package com.nripesh.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+
+@Data
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private  String CategoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
